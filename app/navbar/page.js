4fad9d6294ad59a1c775ec83974896d2293f7622
Sodiqbhoy1 <adamubabasodiq@gmail.com>
2025-02-20 16:44:32 +0100

    Update Navbar link to redirect to homepage

--- app/navbar/page.js
+++ app/navbar/page.js
@@ -47,7 +47,7 @@ export default function Navbar() {
           } absolute md:static top-16 left-0 w-full md:w-auto bg-black md:bg-transparent z-10`}
         >
           <Link
-            href="/signup"
+            href="/"
             className="block md:inline-block px-6 py-2 rounded-lg transition hover:text-white"
           >
             Home
